caf63bab7c15009ae1424e4e95f4323844bee65c
ju0204 <p1106000@gmail.com>
2026-05-13 16:17:55 +0900

    [fix] main-page 수정

--- src/pages/about/ceo.js
+++ src/pages/about/ceo.js
@@ -4,7 +4,6 @@ import CompanyTabs from './companyTabs';
 import DasanImage from '../image/dasan.png';
 import "./ceo.css";
 
-const COMPANY_KR = "대건이앤에스";
 const CEO_NAME = "박 정 이";
 
 export default function CeoMessage() {
@@ -33,18 +32,18 @@ export default function CeoMessage() {
                 안녕하십니까?
               </p>
               <p>
-                대건이앤에스는 산불소화시설 설치 전문 기업으로 사람과 자연의 공존을 최우선 가치로 삼습니다.
+                대건이앤에스는 산불소화시설 설치 전문 기업으로서, 사람과 자연의 조화로운 공간을 최우선 가치로 추구합니다.
               </p>
               <p>
-                최고의 기술력이 최고의 안전을 만든다는 믿음으로 오랜 기간 축적된 기술력을 바탕으로 산불 예방 및 진화 시스템에 앞장서고 있습니다.
+                '<span className="p-best">최고의 기술력이 최고의 안전을 만든다</span>'는 확실한 신념을 가지고 있습니다. 오랜 기간 축적해 온 독보적인 기술력을 바탕으로 스마트한 산불 예방 및 초기진화 시스템 구축에 앞장서고 있습니다.
               </p>
               <p>
-                지속 가능한 자연과 미래 세대에게 물려줄 소중한 환경을 보호한다는 공익적 역할과 공공의 안전을 실현한다는 자부심으로 정직과 신뢰를 바탕으로 항상 최선을 다할 것을 약속 드립니다.
+                지속 가능한 자연을 보존하고 미래 세대에게 물려줄 소중한 환경을 보호한다는 공익적 사명감을 잊지 않고, 공공의 안전을 실현한다는 자부심을 바탕으로, 정직과 신뢰를 통해, 고객에게 항상 최선을 다하는 기업이 될 것을 약속 드립니다.
               </p>
             </div>
 
             <div className="ceo-sign">
-              <span className="sign-company">(주){COMPANY_KR} 대표</span>
+              <span className="sign-company"> 대표</span>
               <span className="sign-name">{CEO_NAME}</span>
             </div>
           </div>
